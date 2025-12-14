0aa457753877391d8d19ce6b8c69fb1431ef6582
kanato1221 <kanatonton2@gmail.com>
2025-12-14 14:11:24 +0900

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by 松井奏人 on 2025/12/14.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
